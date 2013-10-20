84a0d451b8a51ae74b998e4adc7556553c36dcd1
thanhtuit27 <thanhtuit27@yahoo.com>
2013-10-20 22:46:20 +0700

    thanhtuit27

--- MvcApplication1/MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to ASfdg dfg dfP.NET MVC!";
             sfdfsdfssdf sdfsda as fas
             return View();
         }
