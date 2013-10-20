cccf72f3762600afc2f5b4896d653a195b699133
thanhtu <djksfhsjkfhs@sfasd.com>
2013-10-20 22:37:28 +0700

    sdfsdfsd

--- MvcApplication1/MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace MvcApplication1.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
 
             return View();
         }
